60499a14776ca15d2ee053d8582c48f20e4d5519
YashDjsonDookun <dookun88@gmail.com>
2020-09-06 16:17:24 +0400

    update

--- .config/.personal-scripts/change_kitty_theme.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-rm -rf ~/.config/kitty/theme.conf
-ln -s ~/.config/kitty/kitty-themes/themes/$1.conf ~/.config/kitty/theme.conf
-
-
--- .config/.personal-scripts/mount_windows.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-sudo mount -t ntfs -o umask=000  /dev/sda1 /run/media/djson
